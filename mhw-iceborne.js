77fc868d38b4649a2d21840ba5563b6ae3045bf6
LinaYamaguchi <li.na.gintama1113@gmail.com>
2019-12-07 20:13:52 +0900

    6th commit

--- mhw-iceborne.js
+++ mhw-iceborne.js
@@ -1,6 +1,12 @@
 "use strict";
 // Please don't delete the 'use strict' line abov
 
-// const moveToRight = () => {
-//   transform: translateX(-200);
-// };
+const moveToRight = () => {
+  const element = document.getElementById("news-slide");
+  element.style.webkitTransform = "translateX(-310px)";
+};
+
+const moveToLeft = () => {
+  const element = document.getElementById("news-slide");
+  element.style.webkitTransform = "translateX(310px)";
+};
